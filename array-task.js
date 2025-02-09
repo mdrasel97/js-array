bbdf6b26386ae305ce977a4de432dcde806c65c4
mdrasel97 <mollarasel972@gmail.com>
2025-02-09 09:33:34 +0600

    array practice task added

--- array-task.js
+++ array-task.js
@@ -15,6 +15,23 @@
 
 
 // task 3
+// let books = ['bangla', 'html', 'css', 'english', 'js', 'react.js'];
+// console.log(books.includes('js'));
+
+
+// task 4
+// let numbers = [20, 10, 50, 40, 25];
+// let names = 'rasel';
+// let number = 20;
+// console.log(Array.isArray(numbers), Array.isArray(names), Array.isArray(number));
+
+
+
+// task 5
+// let friends = ['rasel', 'hasan', 'kalu', 'malu'];
+// let friend = [20, 25, 24, 26, 20];
+// let concat = friends.concat(friend);
+// console.log(concat);
 
 
 
