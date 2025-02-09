7bc05497e1bdc3f2d9c67c5895c4b4372d699ed1
mdrasel97 <mollarasel972@gmail.com>
2025-02-09 09:11:37 +0600

    array task added

---
+++ array-task.js
@@ -0,0 +1,20 @@
+// task 1
+// let fruits = ['mango', 'banana', 'apple', 'lichu', 'peyara'];
+// console.log(fruits[3]);
+// fruits[2] = 'jambura';
+// console.log(fruits);
+
+
+// task 2
+// let turist = ['cox`s bazar', 'bandarban', 'sundonban'];
+// turist.push('sunar g a o')
+// turist.push('madobkundo', 'kuwakata')
+// console.log(turist);
+// turist.pop(2, 4)
+// console.log(turist);
+
+
+// task 3
+
+
+
---
+++ array.js
@@ -0,0 +1,117 @@
+
+// let number = parseFloat(prompt('enter a number'));
+// if(isNaN(number)){
+//     console.log('please enter a valid number');
+//     return;
+// }
+
+// else if(number % 2 === 0){
+//     console.log(`${number} is even number`)
+// }
+
+// else{
+//     console.log(`${number} is odd number`)
+// }
+
+// simple array 
+
+// let numbers = [50, 40, 65, 55, 70];
+// let friends = ['khalil', 'jolil', 'jubayer', 'ostad'];
+// // let check = [ true, false, true, false]
+
+
+// let friends = ['khalil', 'jolil', 'jubayer', 'ostad'];
+// console.log(friends.length);
+
+
+
+// let friends = ['khalil', 'jolil', 'jubayer', 'ostad'];
+// friends[2] = 'alo';
+// console.log(friends);
+// console.log(friends[2]);
+
+
+
+// push 
+// let friends = ['khalil', 'jolil', 'jubayer', 'ostad'];
+// friends.push('rasel', 'panda')
+// console.log(friends);
+
+
+
+// pop
+// let friends = ['khalil', 'jolil', 'jubayer', 'ostad', 'lamiya', 'samiya'];
+// friends.pop();
+// console.log(friends);
+
+
+
+
+// let friends = ['khalil', 'jolil', 'jubayer', 'ostad', 'lamiya', 'samiya'];
+// console.log(friends.includes('kg'));
+// let nayok = 'mithun';
+// if(nayok == 'hero alom'){
+//     console.log('sala marbo boguray las porbe maguray');
+// }
+// else if(nayok == 'mithun'){
+//     console.log('sala marbo akhane las porbe sosane');
+// }
+// else{
+//     console.log('mara mari valo na')
+// }
+
+// let user = 'rasel'
+// let numbers = [10, 5, 8, 20, 15];
+// console.log(numbers.length);
+
+// let arr = [1, 2, 3, 2, 4, 1, 5, 3];
+// arr.pop();
+// arr.push(20)
+// console.log(arr);
+
+// let numbers = [1, 4, 7, 10, 5, 8];
+// console.log(numbers.includes(5));
+
+
+// const numbers = [10, 20, 30, 40, 50];
+// const target = 30;
+// console.log(numbers.indexOf(30))
+
+// let friends = ['rasel', 'babul', 'kabul']
+// console.log(friends.join())
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let final = arr1.concat(arr2);
+// console.log(final)
+
+
+// let arr = [1, 2, 34, 5, 6];
+// console.log(arr.slice(2, 4))
+
+
+// let email = '';
+// let password = '1234'
+// if(email && password){
+//     console.log('logined');
+// }
+// else{
+//     console.log('please filup input fild')
+// }
+
+// let theme = 'dark';
+// switch (theme) {
+//     case "light":
+//     console.log('light theme applied');
+//     break;
+
+//     case "dark":
+//         console.log('dark theme applied');
+//         break;
+
+
+//         default:
+//             console.log('system is invalid');
+// }
+
+
